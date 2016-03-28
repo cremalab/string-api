31aef207c7899d4321a7cdc0e4f7551628c9e668
Ross Brown <ross@cremalab.com>
2016-03-28 16:16:41 -0500

    fixing dumb error

--- app/controllers/locations_search_controller.js
+++ app/controllers/locations_search_controller.js
@@ -1,12 +1,12 @@
 const Joi    = require('joi'),
       Boom   = require('boom'),
+      Places = require('../lib/places'),
       List   = require('../models/list').List
-      Places = require('../lib/places')
 
 exports.getPredictions = {
   handler: (request, reply) => {
     Places.autoComplete(request.params.search).then((results) => {
-      reply(results: results)
+      reply({results: results})
     }, (err) => {
       reply(Boom.badImplementation(err))
     })
