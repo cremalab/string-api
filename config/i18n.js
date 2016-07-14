3cd4af4f1d03f70e02e2e0b6ff285be13ee553b1
Ross Brown <ross@cremalab.com>
2016-07-14 16:38:03 -0500

    fixing i18n method

--- config/i18n.js
+++ config/i18n.js
@@ -2,10 +2,8 @@ const en = require('./locales/en')
 
 module.exports = {
   t: (key, values) => {
-    console.log(en[key]);
     return Object.keys(values).reduce((mem, v) => {
-      console.log(mem);
-      return mem.replace(`{${v}}`, v)
+      return mem.replace(`{${v}}`, values[v])
     }, en[key])
   }
 }
