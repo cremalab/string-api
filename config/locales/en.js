a236ffc30f2268a38ff7dcd9251478fcd83080fc
Ross Brown <ross@cremalab.com>
2016-07-15 11:50:33 -0500

    adding message schemas

--- config/locales/en.js
+++ config/locales/en.js
@@ -1,4 +1,5 @@
 module.exports = {
   'strings.suggest_location': `Why don't you try {name}? It has {rating} stars`,
+  'strings.suggest_location_again': `Alright, let's try again. Why don't you try {name}? It has {rating} stars`,
   'strings.ask_party_type': `Who are you going with?`
 }
---
+++ config/schemas/botMessageSchema.js
@@ -0,0 +1,25 @@
+const Joi     = require('joi')
+
+const botMessageSchema = Joi.object({
+  text: Joi.string().required().example('I am String and I am cool.'),
+  date: Joi.date().required(),
+  id: Joi.string().required().example(),
+  position: Joi.string().valid(['left', 'right', 'center']).required().example('right'),
+  sentiment: Joi.string().valid(['friendly', 'sad', 'annoyed']).example('friendly'),
+  name: Joi.string().required().valid("String").example('String'),
+  media: Joi.array().items(Joi.object({
+    type: Joi.string().required().example('image'),
+    data: Joi.any().required().example('http://www.silly-string.com/images/logo.png')
+  })).optional(),
+  responseOptions: Joi.object({
+    items: Joi.array().items(Joi.object({
+      text: Joi.string().required().example('Eat'),
+      paramName: Joi.string().required().example('activity_type'),
+      paramValue: Joi.string().required().example('eat')
+    })).optional(),
+    multi: Joi.boolean().required().default(false)
+  }),
+  responseAction: Joi.string().optional().example('string.type')
+})
+
+module.exports botMessageSchema
