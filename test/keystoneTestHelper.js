74e511827945b01d5107c76b1d17d89c8c294d98
Ross Brown <ross@cremalab.com>
2016-09-07 10:30:28 -0500

    fixing test helper

--- test/keystoneTestHelper.js
+++ test/keystoneTestHelper.js
@@ -1,21 +1,21 @@
 'use strict'
 
-const keystone = require('../keystone.js');
+const keystone = require('../keystone.js')
 
-const port = process.env.TEST_PORT || 5150;
-keystone.set('port', port);
-keystone.set('mongo','mongodb://localhost/string_api_test');
+const port = process.env.TEST_PORT || 5150
+keystone.set('port', port)
+keystone.set('mongo','mongodb://localhost/string_api_test')
 
-const swaggerTools = require('swagger-tools');
-const swaggerDoc   = require('../swagger.json');
+const swaggerTools = require('swagger-tools')
+const swaggerDoc   = require('../swagger.json')
 
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  keystone.app.use(middleware.swaggerMetadata());
+  // keystone.app.use(middleware.swaggerMetadata());
 
   // Start the server
   keystone.start()
-});
+})
 
-module.exports = keystone;
+module.exports = keystone
