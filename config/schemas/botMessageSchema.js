9dbd0f75698da408a270d42fb3b18b9850f23cee
Ross Brown <ross@cremalab.com>
2016-09-14 12:46:59 -0500

    removing text requirement on bot message

--- config/schemas/botMessageSchema.js
+++ config/schemas/botMessageSchema.js
@@ -1,7 +1,7 @@
 const Joi     = require('joi')
 
 const botMessageSchema = Joi.object({
-  text: Joi.string().required().example('I am String and I am cool.'),
+  text: Joi.string().optional().example('I am String and I am cool.'),
   date: Joi.date().required(),
   id: Joi.string().required(),
   position: Joi.string().valid(['left', 'right', 'center']).required().example('right'),
