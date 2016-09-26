b787ac7cfe972bf38e03cba2917d40fe762cb4cc
Ross Brown <ross@cremalab.com>
2016-09-26 13:53:58 -0500

    updating contentTagger to use taggers when tagging activities

--- models/tagger.js
+++ models/tagger.js
@@ -15,8 +15,14 @@ Tagger.add({
     type: Types.Text, note: `The tag to be added on keyword match`,
     required: true, initial: true
   },
+  activity_types: {
+    type: Types.TextArray, required: true, initial: true,
+    note: `will only be applied if activity is of one of these types`,
+    default: []
+  },
   keywords: {
     type: Types.TextArray, required: true, initial: true,
+    default: [],
     note: `if any of these are included in the activity description, this Tagger's tag will be added`
   }
 })
